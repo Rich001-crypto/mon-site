60f8faa3bf5b0094f1cc26b1d52638f6dbc29abd
Rich001-crypto <richardkeukeu@gmail.com>
2026-09-14 01:39:42 +0200

    Correction de l'import accordion

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { ChevronDownIcon } from "lucide-react"
 import { Accordion as AccordionPrimitive } from "radix-ui"
 
-import { cn } from "@radix-ui/react-accordion"
+import { Accordion as AccordionPrimitive } from "@radix-ui/react-accordion";
 
 function Accordion({
   ...props
